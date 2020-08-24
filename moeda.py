68c808407fff4bffcb3d49aab66c895b707a39fe
ygorferreira4g <65570661+ygorferreira4g@users.noreply.github.com>
2020-08-24 18:13:59 -0300

    exercíci que faltava

---
+++ moeda.py
@@ -0,0 +1,17 @@
+md5 = input  ('qtd md 5:')
+md10 = input ('qtd md 10:')
+
+def calk(): 
+    clc = float(md5)*(5)/(20) 
+    clc2 = float(md10)*(10)/(10)
+    nt2 = float(clc)+float(clc2)
+
+
+print ("qtd nt 2: {0:.2f}".format(nt2))
+
+
+calk()
+    
+
+
+
