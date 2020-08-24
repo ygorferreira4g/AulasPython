4b1fa1bc4e5101abb0b28b7e893eb412826bb72f
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-24 18:28:18 -0300

    Endentação da função não feita #1

--- moeda.py
+++ moeda.py
@@ -1,14 +1,12 @@
-md5 = input  ('qtd md 5:')
-md10 = input ('qtd md 10:')
+md5 = input('qtd md 5:')
+md10 = input('qtd md 10:')
 
 def calk(): 
-    clc = float(md5)*(5)/(20) 
-    clc2 = float(md10)*(10)/(10)
-    nt2 = float(clc)+float(clc2)
+    clc = (float(md5)*(5))/(20) 
+    clc2 = (float(md10)*(10))/(10)
+    nt2 = clc+clc2
 
-
-print ("qtd nt 2: {0:.2f}".format(nt2))
-
+    print ("qtd nt 2: {0:.2f}".format(nt2))
 
 calk()
     
