a4474668ed2ff767fe88f7f4c6af66447d45d40c
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-08-27 22:40:29 -0300

    Ajustes tempo de viagem #3

--- exercicios_Python2/exer8.py
+++ exercicios_Python2/exer8.py
@@ -8,11 +8,14 @@ qtd_L = Km/km_l
 
 tempo_Gasto = Km/velocidade_Média
 
+horasGastas = int(tempo_Gasto)
+minutosGastos = int((tempo_Gasto - int(tempo_Gasto))*60)
+
 reais = qtd_L * 4.60
 
 
 
 print("Combustível: %.2f"  % (qtd_L ),"Litros")
-print("Duração de viagem" ,tempo_Gasto, "Horas")
+print("Duração de viagem" ,horasGastas,"Horas e",minutosGastos,"Minutos")
 print("Valor gasto com Combustível: R$ %.2f" %  (reais))
 
